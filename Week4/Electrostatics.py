0f8e8955902bce7d7557465e46509a014430bb14
Kyle Boucher <kjbouch@gmail.com>
2013-05-07 14:05:28 -0700

    new function in electrostatics

--- Week4/Electrostatics.py
+++ Week4/Electrostatics.py
@@ -52,3 +52,17 @@ def dipolePotential(x,y,q,d,a,k=8.987552e9,ang=0.0,xc=0.0,yc=0.0):
         s = 1
     V = pointPotential(x,y,q,xo+xc,yo+yc,k) + pointPotential(x,y,-q * s,-xo+xc,-yo+yc)
     return V
+
+def pointField(x,y,q,xo=0.0,yo=0.0,k=8.987552e9,denominator_r_mag_power=3.0):
+    """Takes two numbers, or two arrays with the same shape, such as two 2D arrays x and y 
+    created from np.meshgrid(). Also take charge of the point particle as a float including sign.
+    Outputs two arrays of the same shape as x and y. The first array output is the x component of the 
+    electric field in the xy plane, the second is the y component.
+    xo and yo are the coordinates of the charge in the xy plane, both default to zero. 
+    k is the coulumb force constant 1/4pi(epsilon_not)defualts to the SI value 8.987552e9 with units (Newton)(meter^2)/(Coulumb^2).
+    Note: this module contains k and the fundamental charge e as constants. (Similar to function pointPotential))
+    """
+    
+    Ex = (k*q)*(x-xo)/(((x-xo)**2.0 + (y-yo)**2)**0.5)**denominator_r_mag_power
+    Ey = (k*q)*(y-yo)/(((x-xo)**2.0 + (y-yo)**2)**0.5)**denominator_r_mag_power
+    return (Ex,Ey)
